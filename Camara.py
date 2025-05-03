80158321ed240ad0a4425f7d5a27e4e42ebd3fbb
Jessua Solis <147453642+Jessuapapu@users.noreply.github.com>
2025-05-03 17:49:11 -0600

    Movimiento de camara con aswd

--- Camara.py
+++ Camara.py
@@ -1,16 +1,16 @@
-
+import pygame
 from OpenGL.GL import *
 from OpenGL.GLU import *
 import math
 class Camaras:
 
         
-    def __init__(self, cam_pos, cam_yaw, velocidad, sensibilidad):
+    def __init__(self, cam_pos:list , cam_yaw:float, velocidad:float, sensibilidad:float):
         """ 
-            Cam_pos es la posicion de la camara
-            Cam_yaw es el eje de vista o de rotacion con respecto a la vista
-            Velocidad es la velocidad con la que se mueve la camara
-            Sensibilidad es el coefinciente para mover la camara
+            Cam_pos es la posicion de la camara,
+            Cam_yaw es el eje de vista o de rotacion con respecto a la vista,
+            Velocidad es la velocidad con la que se mueve la camara,
+            Sensibilidad es el coefinciente para mover la camara,
             
         """
         self.cam_pos = cam_pos
@@ -18,6 +18,7 @@ class Camaras:
         self.velocidad = velocidad
         self.sencibilidad = sensibilidad
         self.cam_target = [] 
+        
         
     def InicializarTarget(self):
         self.cam_target = [
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from OpenGL.GL import *
 from OpenGL.GLU import *
 import math
 
-# se carga las librerias que funcionan para cargar y guadar los modelos
+# se carga los modelos de clases para una mejor estructura
 import Modelos
 import Camara
 
@@ -15,56 +15,38 @@ def main():
     pygame.display.set_caption("Modo caminar con gluLookAt")
 
     glEnable(GL_DEPTH_TEST)
+    glMatrixMode(GL_PROJECTION)
     gluPerspective(45, (display[0] / display[1]), 0.1, 700.0)
+    glMatrixMode(GL_MODELVIEW)
 
     modelo = Modelos.Modelo('modelosObj/Hacienda.obj')
-    camara = Camara.Camaras([0.0, 1.0, -250.0], 0.0 , 1, 2.0)
+    camara = Camara.Camaras([0.0, 1.0, -100.0], 0.0 , 0.54 , 2.0)
     clock = pygame.time.Clock()
 
     running = True
     while running:
-        clock.tick(60)
+        clock.tick(120)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
                 
-            if event.type == pygame.MOUSEBUTTONDOWN:
+            """if event.type == pygame.MOUSEBUTTONDOWN:
                 mouse_presses = pygame.mouse.get_pressed()
                 if mouse_presses[0]:
-                    print("Left Mouse key was clicked" )    
-
-
+                    print("Left Mouse key was clicked" ) """   
+                
         keys = pygame.key.get_pressed()
-
-
-        # Rotaciones
-        if keys[K_LEFT]:
-            camara.cam_yaw -= camara.sencibilidad
-        if keys[K_RIGHT]:
-            camara.cam_yaw += camara.sencibilidad
-
-        # Movimiento adelante / atrás
-        sin_yaw = math.sin(math.radians(camara.cam_yaw))
-        cos_yaw = math.cos(math.radians(camara.cam_yaw))
-
-        if keys[K_w]:  # Avanzar
-            camara.cam_pos[0] += sin_yaw * camara.velocidad
-            camara.cam_pos[2] -= cos_yaw * camara.velocidad
-            print(camara.cam_pos)
-        if keys[K_s]:  # Retroceder
-            camara.cam_pos[0] -= sin_yaw * camara.velocidad
-            camara.cam_pos[2] += cos_yaw * camara.velocidad
-            print(camara.cam_pos)
-        if keys[K_a]:  # Izquierda
-            camara.cam_pos[0] -= cos_yaw * camara.velocidad
-            camara.cam_pos[2] -= sin_yaw * camara.velocidad
-            print(camara.cam_pos)
-        if keys[K_d]:  # Derecha
-            camara.cam_pos[0] += cos_yaw * camara.velocidad
-            camara.cam_pos[2] += sin_yaw * camara.velocidad
-            print(camara.cam_pos)
-            
+        
+        
+        if keys[K_d]:  # Avanzar
+            camara.cam_pos[0] += 0.5 * camara.velocidad
+        if keys[K_a]:
+            camara.cam_pos[0] += -0.5 * camara.velocidad 
+        if keys[K_w]:
+            camara.cam_pos[2] += -0.5 * camara.velocidad 
+        if keys[K_s]:
+            camara.cam_pos[2] += 0.5 * camara.velocidad 
         
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
         glLoadIdentity()
